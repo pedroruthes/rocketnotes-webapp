f04361d8894ff8a9b244adbbc5f928bc5399193b
Pedro Ruthes <pedroo.ruthes@gmail.com>
2024-11-10 16:23:45 -0300

    feat: session + update profile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,31 @@
-import { RiShutDownLine } from "react-icons/ri"
+import { RiShutDownLine } from "react-icons/ri";
+import { useAuth } from "../../hooks/auth";
+
+import { api } from "../../services/api";
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+
 import { Container, Profile, Logout } from "./styles";
 
 export default function Header() {
+    const { signOut, user } = useAuth();
+
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
     return (
         <Container>
             <Profile to="/profile">
                 <img 
-                    src="https://github.com/pedroruthes.png"
-                    alt="Foto do usuário"
+                    src={avatarUrl}
+                    alt={user.name}
                 />
 
                 <div>
                     <span>Bem-vindo</span>
-                    <strong>Pedro Ruthes</strong>
+                    <strong>{user.name}</strong>
                 </div>
             </Profile>
             
-            <Logout>
+            <Logout onClick={signOut}>
                 <RiShutDownLine />
             </Logout>
         </Container>
